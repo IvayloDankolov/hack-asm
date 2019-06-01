d7a9b56b6630a8ef3b0a2ecd41e842c1068d1453
Ivaylo Dankolov <IvoDankolov@gmail.com>
2019-06-01 15:59:24 +0300

    Command line

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,22 @@
-console.log("Hello world!");
+#!/usr/bin/env node
+
+import * as yargs from 'yargs';
+
+yargs
+.scriptName("hack-asm")
+.usage("$0 <source>")
+.command({
+    command: "compile <source>", 
+    aliases: ["compile", '*'],
+    describe: "comiple a Hack assembly file", 
+    builder: yargs => yargs.positional('source', {
+        describe: 'Name of the source file written in Hack assembly',
+        type: 'string',
+    }),
+    handler: (argv) => {
+        const source = argv.source;
+        console.log(source);
+    }
+})
+.help()
+.argv;
