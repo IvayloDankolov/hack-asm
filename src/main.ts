976b36e03105005f156a38412cb7cb72e402f7a6
Ivaylo Dankolov <IvoDankolov@gmail.com>
2019-06-03 23:40:56 +0300

    Verbose mode that shows timings and potentially other useful stuff later

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,9 @@ import { compile } from './compile';
 import { binaryTranslate } from './binary-translate';
 import * as path from 'path';
 import * as fs from 'mz/fs';
+import moment from 'moment';
+import Table from 'cli-table';
+import { getTiming } from './tools';
 
 yargs
 .scriptName("hack-asm")
@@ -14,12 +17,20 @@ yargs
     command: "compile <source> [dest]", 
     aliases: ["compile", '*'],
     describe: "comiple a Hack assembly file", 
-    builder: yargs => yargs.positional('source', {
+    builder: yargs => yargs
+    .positional('source', {
         describe: 'Name of the source file written in Hack assembly',
         type: 'string',
     })
     .positional('dest', {
         describe: "Where to save the resulting binary code. Will default to $sourceBasename.hack"
+    })
+    .option("verbose", {
+        alias: 'v',
+        describe: "Show timings and other useful information",
+        required: false,
+        type: 'boolean',
+        default: false,
     }),
     handler: async (argv) => {
         const source = argv.source as string;
@@ -28,12 +39,39 @@ yargs
             const p = path.parse(source);
             dest = path.join(p.dir, p.name + '.hack');
         }
+
+        const verbose = argv.verbose as boolean;
         try {
-            const program = await compile(source);
+            const startTime = moment();
+            let refTime = moment();
+            const timings = new Table({
+                chars: {},
+                head: ["time", "operation"]
+            });
             
+            const program = await compile(source);
+
+            const compileSpeed = getTiming(refTime, moment());
+            timings.push([compileSpeed, "parsed and compiled"])
+            refTime = moment();
+
             const buf = binaryTranslate(program);
 
+            const translationSpeed = getTiming(refTime, moment());
+            timings.push([translationSpeed, "binary translation"]);
+            refTime = moment();
+
             await fs.writeFile(dest, buf.toBuffer());
+
+            const writeSpeed = getTiming(refTime, moment());
+            timings.push([writeSpeed, "write destination"])
+
+            const total = getTiming(startTime, moment());
+            timings.push([total, "total time"]);
+
+            if(verbose) {
+                console.log(timings.toString());
+            }
         } catch(e) {
             console.log(e.message);
             process.exit(1);
--- src/tools.ts
+++ src/tools.ts
@@ -1,3 +1,10 @@
+import moment = require("moment");
+
 export type Dict<T> = {
     [key: string]: T;
+}
+
+export function getTiming(start: moment.Moment, end: moment.Moment) {
+    const duration = moment.duration(end.diff(start));
+    return `${duration.seconds()}.${duration.milliseconds().toString().padStart(3, '0')}s`;
 }
